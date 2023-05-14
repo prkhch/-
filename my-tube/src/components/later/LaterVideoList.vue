3c4068cfcbccd98f34d265a19ddfce79b6911a7e
PARKHEECHANG <shjc4623@gmail.com>
2023-05-15 00:24:55 +0900

    VUE2_YouTube API를 활용한_스트리밍_플랫폼

--- my-tube/src/components/later/LaterVideoList.vue
+++ my-tube/src/components/later/LaterVideoList.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    <h1>LaterVideo</h1>
+    <ul class="video-list-ul">
+      <li class="video-list-li" v-for="video in laterVideoList" v-bind:key="video.videoId">
+        <EachVideo v-bind:video="video" />
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+import EachVideo from "../common/EachVideo.vue";
+
+export default {
+  components: {
+    EachVideo,
+  },
+  props: {
+    laterVideoList: Array,
+  },
+};
+</script>
