25ccf16e2d9bf421682e53850fd71f5cd4eea5c6
PARKHEECHANG <shjc4623@gmail.com>
2023-05-28 10:00:46 +0900

    ReACT

--- React/hello-react/src/App.js
+++ React/hello-react/src/App.js
@@ -1,27 +1,48 @@
 // 원래 브라우저에서 모듈을 불러오는 기능은 없다. Node.js에 지원하..
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 // 함수 컴포넌트
 // 이건 HTML도 문자열 템플릿도 아닌 JSX.
-function App() { 
+function App() {
+  // 자바스크립트 표현식 가능
+  // const name = undefined;
+  // return <div>{ name || '이름을 다시 입력하세요.'}</div>
+
+  // const name =     '리액트'
+  // const style = {
+  //   backgroundColor : 'black',
+  //   color : 'aqua',
+  //   fontSize : '48px',
+  //   fontWeight : 'bold',
+  //   padding : 16
+  // };
+  // return <div style={style}>{name}</div>
+
+  // const name = '리액트';
+  // return <div className="react">{name}</div>;
+
+  //   const name = '리액트';
+  //   return (
+  //     <>
+  //       <div className='react'>{name}</div>
+  //       <input/>
+  //       {/* 주석다는법 */}
+  //       {/* 루트 요소 안에서 주석 */}
+  //       // 이건 주석이 아니야
+  //       /* 이것도 */
+  //     </>
+  //   );
+  // }
+
+  const name = "이상 한 들여 쓰 기";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="react">{name}</div>
+      <h1>들여쓰기</h1>
+      <h2>이상 </h2>
+      <p>할때는</p>
+      <p>f1 = &gt; code-prettier</p>
+    </>
   );
 }
 
