10bbbed6f67da2d94f4b7abf1cda1d92c0b1eb9f
PARKHEECHANG <shjc4623@gmail.com>
2023-05-28 21:39:15 +0900

    ~3.4

--- React/hello-react/src/MyComponents.js
+++ React/hello-react/src/MyComponents.js
@@ -15,27 +15,68 @@
 
 // export default MyComponent;
 
+// propTypes를 통한 props 검증
+// import PropTypes from 'prop-types';
+
+// const MyComponent = ({ name, favoriteNumber, children }) => {
+//   return (
+//     <div>
+//       컴포넌트 이름 {name}
+//       <br />
+//       자식 {children}
+//       <br />
+//       숫자는 {favoriteNumber}
+//     </div>
+//   );
+// };
+
+// MyComponent.defaultProps = {
+//   name: '기본 이름',
+// };
+
+// MyComponent.propTypes = {
+//   name: PropTypes.string, // (타입을 통한) 검증
+//   favoriteNumber: PropTypes.number.isRequired,
+// };
+
+// export default MyComponent;
+
+// 클래스형 컴포넌트에서 props
+import { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const MyComponent = ({ name, favoriteNumber, children }) => {
-  return (
-    <div>
-      컴포넌트 이름 {name}
-      <br />
-      자식 {children}
-      <br />
-      숫자는 {favoriteNumber}
-    </div>
-  );
-};
+class MyComponent extends Component {
+  static defaultProps = {
+    name: '기본 이름',
+  };
+  static propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired,
+  };
 
-MyComponent.defaultProps = {
-  name: '기본 이름',
-};
+  render() {
+    const { name, favoriteNumber, children } = this.props; // 비구조화  할당
+    return (
+      <div>
+        안녕하세요. 이름 {name}
+        <br />
+        children은 {children}
+        <br />
+        숫자는 {favoriteNumber}
+      </div>
+    );
+  }
+}
 
-MyComponent.propTypes = {
-  name: PropTypes.string, // (타입을 통한) 검증
-  favoriteNumber: PropTypes.number.isRequired,
-};
+//  클래스형 컴포넌트에서는 class 내부에서 타입 기본값 지정 가능
+
+// MyComponent.defaultProps = {
+//   name: '기본 이름',
+// };
+
+// MyComponent.propTypes = {
+//   name: PropTypes.string,
+//   favoriteNumber: PropTypes.number.isRequired,
+// };
 
 export default MyComponent;
