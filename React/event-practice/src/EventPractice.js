252a82c3e924b4c2c8963264de133031a13b41a6
PARKHEECHANG <shjc4623@gmail.com>
2023-05-30 22:24:55 +0900

    event-handling

--- React/event-practice/src/EventPractice.js
+++ React/event-practice/src/EventPractice.js
@@ -2,28 +2,29 @@ import { Component } from "react";
 
 class EventPractice extends Component {
   state = {
+    username: "",
     message: "",
   };
 
-  constructor(props) {
-    // 함수 바인딩 작업
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleChange(e) {
+  handleChange = (e) => {
     this.setState({
-      message: e.target.value, // 2. state의 message값 변경
+      [e.target.name]: e.target.value, // 2. state의 message값 변경
     });
-  }
+  };
 
-  handleClick() {
-    alert(this.state.message);
+  handleClick = () => {
+    alert(this.state.username + " : " + this.state.message);
     this.setState({
+      username: "",
       message: "",
     });
-  }
+  };
+
+  handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      this.handleClick();
+    }
+  };
 
   render() {
     return (
@@ -31,10 +32,18 @@ class EventPractice extends Component {
         <h1>이벤트 연습</h1>
         <input
           type="text"
+          name="username"
+          placeholder="이름"
+          value={this.state.username} // 3. 그 message값을 value로
+          onChange={this.handleChange} // 1.this.handleChange
+        />
+        <input
+          type="text"
           name="message"
-          placeholder="플레이스 홀더"
+          placeholder="메시지"
           value={this.state.message} // 3. 그 message값을 value로
           onChange={this.handleChange} // 1.this.handleChange
+          onKeyPress={this.handleKeyPress}
         />
         <button onClick={this.handleClick}>확인</button>
       </div>
