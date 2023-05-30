839c10b1692d168e904d8cdf024f3c31a702faa1
PARKHEECHANG <shjc4623@gmail.com>
2023-05-30 23:14:43 +0900

    event-handling

---
+++ React/event-practice/src/EventPractice2.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+// 함수 컴포넌트
+const EventPractice2 = () => {
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState("");
+  const onChangeUsername = (e) => setUsername(e.target.value);
+  const onChangeMessage = (e) => setMessage(e.target.value);
+  const onClick = () => {
+    alert(username + ":" + message);
+    setUsername("");
+    setMessage("");
+  };
+
+  const onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      onClick();
+    }
+  };
+
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        value={username}
+        onChange={onChangeUsername}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="내용"
+        value={message}
+        onChange={onChangeMessage}
+        onKeyPress={onKeyPress}
+      />
+      <button onClick={onClick}>확인</button>
+    </div>
+  );
+};
+
+export default EventPractice2;
