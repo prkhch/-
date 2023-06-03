5c0c9c1784d9b350d16f14c42dec31e23a130756
PARKHEECHANG <shjc4623@gmail.com>
2023-06-03 20:28:02 +0900

    lifecycle-method

--- React/lifecycle-method/src/App.js
+++ React/lifecycle-method/src/App.js
@@ -1,14 +1,34 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Component } from "react";
+import LifeCycleSample from "./LifeCycleSample";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <button onClick={this.handleClick}>랜덤 색상</button>
+          <LifeCycleSample color={this.state.color} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
