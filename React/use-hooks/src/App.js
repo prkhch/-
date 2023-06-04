fbc91b9c0c8af4c22391d42f80b31b26edb23dc6
PARKHEECHANG <shjc4623@gmail.com>
2023-06-04 21:54:15 +0900

    hooks

--- React/use-hooks/src/App.js
+++ React/use-hooks/src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Counter from "./Counter";
+import Info from "./Info";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
+        <Info />
       </header>
     </div>
   );
