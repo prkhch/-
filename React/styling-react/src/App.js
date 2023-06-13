b596354227140f09a0a029a74bf2bc90fc6075df
PARKHEECHANG <shjc4623@gmail.com>
2023-06-13 16:59:36 +0900

    Commit before yarn eject

--- React/styling-react/src/App.js
+++ React/styling-react/src/App.js
@@ -1,23 +1,18 @@
 import { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import SassComponent from "./SassComponent";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <SassComponent />
         <header>
           <img src={logo} className="logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
