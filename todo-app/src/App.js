2f9ad82b5970163818bee50995905cfa0f1d9be8
PARKHEECHANG <shjc4623@gmail.com>
2023-06-14 16:14:23 +0900

    리액트-TodoList

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,46 @@
+import { useState, useRef, useCallback } from 'react';
 import TodoInsert from './components/TodoInsert';
 import TodoTemplate from './components/TodoTemplate';
 import TodoList from './components/TodoList';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트 기초',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱',
+      checked: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert />
-      <TodoList />
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
