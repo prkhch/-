f81aa44cd13833cce20debfef0818251e6673620
PARKHEECHANG <shjc4623@gmail.com>
2023-06-16 19:32:22 +0900

    REACT-일정관리 웹 애플리케이션

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -5,7 +5,7 @@ import TodoList from './components/TodoList';
 
 function createBulkTodos() {
   const array = [];
-  for (let i = 1; i <= 10; i++) {
+  for (let i = 1; i <= 30; i++) {
     array.push({
       id: i,
       text: `할 일 ${i}`,
