e9d22ba4319281955e64817492a1922c3256e145
PARKHEECHANG <shjc4623@gmail.com>
2023-06-16 09:30:21 +0900

    REACT-일정관리 애플리케이션

--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -6,12 +6,12 @@ import {
 import cn from 'classnames'; // 조건부 스타일링
 import './TodoListItem.scss';
 
-const TodoListItem = ({ todo, onRemove }) => {
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
   const { id, text, checked } = todo;
 
   return (
     <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })}>
+      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
         {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
         <div className="text">{text}</div>
       </div>
