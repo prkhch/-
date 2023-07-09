7d199c144c84f639b9d26e5d36e38acf41e65c50
PARKHEECHANG <shjc4623@gmail.com>
2023-07-10 02:11:46 +0900

    REACT-WebRTC-VideoChat

---
+++ web-rtc-chat-app/README.md
@@ -0,0 +1,11 @@
+npm init -y
+
+npm install cors express nodemon socket.io
+
+npx create-react-app ./client
+
+npm i @material-ui/core --legacy-peer-deps
+
+npm i @material-ui/icons --legacy-peer-deps
+
+npm install react-copy-to-clipboard simple-peer socket.io-client
---
+++ web-rtc-chat-app/client/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+import { ContextProvider } from "./SocketContext";
+import "./styles.css";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>
+);
