e2b6b082b4b6c55ab3cf329312e0a96b9afab2e2
PARKHEECHANG <shjc4623@gmail.com>
2023-07-10 09:34:56 +0900

    REACT-APP

--- React/event-practice/src/App.js
+++
@@ -1,8 +0,0 @@
-import "./App.css";
-import EventPractice2 from "./EventPractice2";
-
-const App = () => {
-  return <EventPractice2 />;
-};
-
-export default App;
--- React/immer-lib/README.md
+++
@@ -1,3 +0,0 @@
-yarn add immer@3.2.1
-
-[![immer](https://img.shields.io/badge/immer-000000?style=for-the-badge&logo=immer&logoColor=white)](#)
