26bbac6b5ef2c7f6cd876bb728556b24c60c1092
박희창 <122577719+PARKHEECHANG@users.noreply.github.com>
2023-07-24 18:53:29 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,17 @@
+def solution(num_list):
+    answer = []
+    
+    a = 0
+    b = 0
+    
+    for i in range(len(num_list)) :
+        if(num_list[i] % 2 ==0) :
+            a+=1
+        else :
+            b+=1
+            
+    answer.append(a)
+    answer.append(b)
+    
+    
+    return answer
