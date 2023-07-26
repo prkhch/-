7cb0e95cd5568e8479c21bb25d7574272d8f740e
박희창 <122577719+PARKHEECHANG@users.noreply.github.com>
2023-07-26 21:24:37 +0900

    [level 2] Title: 타겟 넘버, Time: 6.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,21 +1,19 @@
-cnt = 0
-
-def run(numbers, target, lv, res) :
-    global cnt
-    
-    if(len(numbers)==lv) :
-        if(res==target) :
-            cnt += 1
-            
-        return
-        
-    run(numbers, target, lv+1, res + numbers[lv])
-    run(numbers, target, lv+1, res - numbers[lv])
-    
 def solution(numbers, target):
+    global c
     answer = 0
-    run(numbers, target, 0, 0)
-    
-    answer = cnt
-    
+    dfs(0,0,numbers,target)
+    answer = c
+        
     return answer
+c = 0
+def dfs(lev,Sum,lst,num):
+    global c
+    if lev == len(lst):
+        if Sum == num:
+            c += 1
+        return
+    for i in range(2):
+        if i ==1:
+            dfs(lev+1,Sum+lst[lev],lst,num)
+        else:
+            dfs(lev+1,Sum-lst[lev],lst,num)
