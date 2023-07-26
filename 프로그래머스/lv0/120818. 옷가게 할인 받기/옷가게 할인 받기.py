65dc4ea3ac84ae39b71282e849aa66c307a7e865
박희창 <122577719+PARKHEECHANG@users.noreply.github.com>
2023-07-26 21:25:58 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,13 +1,9 @@
 def solution(price):
-    answer = 0
-    
-    if(price >= 500000) :
-        answer = price * 0.80;
-    elif(price >= 300000) :
-        answer = price * 0.90;
-    elif(price >= 100000) :
-        answer = price * 0.95;
-    else :
-        answer = price
-        
-    return int(answer)
+    if price >= 500000:
+        price *= 0.8
+    elif price >= 300000:
+        price *= 0.9
+    elif price >= 100000:
+        price *= 0.95
+        price = int(price)
+    return price
