7b19976dea0a68278d94342023df001871fe4017
박희창 <122577719+PARKHEECHANG@users.noreply.github.com>
2023-08-12 14:39:28 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 14 ms, Memory: 13,548 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.cpp
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.cpp
@@ -1,44 +1,31 @@
 #include <iostream>
 #include <string>
-#include <bitset>
+
 using namespace std;
 
-// 각 자릿수에 숫자를 비트로 변경
-// ex) 0은 1 << 0 == 1
-// ex) 1은 1 << 1 == 10
-// ex) 2은 1 << 2 == 100
-// ex) 2은 1 << 3 == 1,000
-// ex) 2은 1 << 4 == 10,000
-// ex) 2은 1 << 5 == 100,000
-// ex) 2은 1 << 6 == 1,000,000
-// ex) 2은 1 << 7 == 10,000,000
-// ex) 2은 1 << 8 == 100,000,000
-// ex) 2은 1 << 9 == 1,000,000,000
-
-// '|'를 통해 누적연산
 
 int main() {
-
-	int testCase;
-	cin >> testCase;
-	int goal = (1 << 10) - 1; // 1,111,111,111
-	
-
-	for (int i = 1; i <= testCase; i++) {
-		int n;
+	int t;
+	int n;
+	cin >> t;
+	for (int i = 1; i <= t; i ++) {
+		int SWITCH = 0;
+		int cnt = 0;
 		cin >> n;
 
-		int visited = 0;
-		int count = 1;
-		while (1) {
-			string str = to_string(n * count);
-			for (char c : str) {
-				int num = c - '0';
-				visited = visited | (1 << num);
+		while (SWITCH != 1023) {
+			cnt += 1;
+			int nn = n * cnt;
+			while (nn > 0) {
+				int digit = nn % 10;
+				SWITCH |= 1 << digit;
+				nn /= 10;
 			}
-			if (visited == goal) break;
-			count++;
 		}
-		cout << '#' << i << " " << n * count << "\n";
+		
+		cout << '#'<<i  << ' ' <<  cnt * n << '\n';
+		
 	}
+
+
 }
