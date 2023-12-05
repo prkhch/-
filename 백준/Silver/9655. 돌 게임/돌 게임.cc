79df7be2a09ab72b7ebcd0f11a1870d326b5b7ca
박희창 <122577719+PARKHEECHANG@users.noreply.github.com>
2023-12-05 13:03:49 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int arr[21];
+int main() {
+	int n;
+	cin >> n;
+
+	if (n % 2 == 0) {
+		cout << "CY";
+	}
+	else {
+		cout << "SK";
+	}
+
+}
