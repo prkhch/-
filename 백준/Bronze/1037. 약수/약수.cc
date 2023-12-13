a57cde32b0324c7ca5d74591421c9ea4e97232aa
박희창 <122577719+PARKHEECHANG@users.noreply.github.com>
2023-12-13 23:38:29 +0900

    [Bronze I] Title: 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+	int N;
+	cin >> N;
+
+	vector<int> v;
+	for (int i = 0; i < N; i++) {
+		int a;
+		cin >> a;
+		v.push_back(a);
+	}
+
+	sort(v.begin(), v.end());
+
+	cout << v[0] * v[N - 1];
+}
