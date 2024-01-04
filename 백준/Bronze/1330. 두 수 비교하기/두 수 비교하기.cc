a715b31a7791f599c2a8fb6f8057530b716afd3f
박희창 <122577719+prkhch@users.noreply.github.com>
2024-01-04 15:34:50 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int A, B;
+
+int main() {
+	
+	cin >> A >> B;
+
+	if (A > B) {
+		cout << ">";
+	} 
+	else if (A < B) {
+		cout << "<";
+	}
+	else {
+		cout << "==";
+	}
+
+}
