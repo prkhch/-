53861e19cd1035a7635a3ac723ac0f8767ea5344
박희창 <122577719+prkhch@users.noreply.github.com>
2024-01-05 11:43:11 +0900

    [Silver IV] Title: 큐, Time: 336 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.cc
+++ 백준/Silver/10845. 큐/큐.cc
@@ -1,65 +1,56 @@
 #include <iostream>
-#include <map>
-#include <vector>
-#include <string>
-#include <algorithm>
 #include <queue>
+
 using namespace std;
 
-int n;
-
 int main() {
-	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
 	queue<int> q;
 
-	cin >> n;
-
-	for (int i = 0; i < n; i++) {
-		string str;
-		cin >> str;
-
-		if (str == "push") {
-			int num;
-			cin >> num;
-			q.push(num);
+	int N;
+	cin >> N;
+	string cmd; int x;
+	for (int i = 0; i < N; i++) {
+		cin >> cmd;
+		if (cmd == "push") {
+			cin >> x;
+			q.push(x);
 		}
-		else if (str == "pop") {
-			if (!q.size()) {
-				cout << "-1" << "\n";
+		else if (cmd == "pop") {
+			if (q.empty()) {
+				cout << -1 << "\n";
 			}
 			else {
 				cout << q.front() << "\n";
 				q.pop();
 			}
 		}
-		else if (str == "size") {
+		else if (cmd == "size") {
 			cout << q.size() << "\n";
 		}
-		else if (str == "empty") {
-			if (!q.size()) {
-				cout << "1" << "\n";
+		else if (cmd == "empty") {
+			if (q.empty()) {
+				cout << 1 << "\n";
 			}
 			else {
-				cout << "0" << "\n";
+				cout << 0 << "\n";
 			}
 		}
-		else if (str == "front") {
-			if (!q.size()) {
-				cout << "-1" << "\n";
+		else if (cmd == "front") {
+			if (q.empty()) {
+				cout << -1 << "\n";
 			}
 			else {
 				cout << q.front() << "\n";
 			}
-		}
-		else if (str == "back") {
-			if (!q.size()) {
-				cout << "-1" << "\n";
+		}		
+		else if (cmd == "back") {
+			if (q.empty()) {
+				cout << -1 << "\n";
 			}
 			else {
 				cout << q.back() << "\n";
 			}
 		}
-		
 	}
-
 }
