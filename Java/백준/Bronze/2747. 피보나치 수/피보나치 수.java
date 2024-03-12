61890d94836d1106ee4c5cc9978a0f53d20de2bd
박희창 <122577719+prkhch@users.noreply.github.com>
2024-03-12 22:51:50 +0900

    [Bronze II] Title: 피보나치 수, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	static int[] arr = new int[46];
+
+	public static void fibo(int n) {
+		for(int i=2; i<=n; i++) {
+			arr[i] = arr[i-1] + arr[i-2];
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		arr[0] = 0;
+		arr[1] = 1;
+		
+		fibo(n);
+		
+		System.out.print(arr[n]);
+	
+	}
+}
