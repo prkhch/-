ff60a1791f9cac8afbb6b50cf9ef578c3140169e
박희창 <122577719+prkhch@users.noreply.github.com>
2024-03-12 19:43:32 +0900

    [Silver II] Title: 스택 수열, Time: 1732 ms, Memory: 42764 KB -BaekjoonHub

---
+++ Java/백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+				
+		int num;
+		int start = 1;
+		ArrayDeque<Integer> dq = new ArrayDeque<>();
+		ArrayDeque<String> res = new ArrayDeque<>();
+//		char[] result = new char[200002];
+		int rs_idx = 0;
+		
+		for(int i=0; i<n; i++) {
+			num = Integer.parseInt(br.readLine());
+			
+			if(num >= start) {
+				for(int j=start; j<=num; j++) {
+					dq.add(j);
+//					result[rs_idx] = '+';
+					res.add("+");
+					rs_idx++;
+				}
+				start = num+1;
+			}
+			
+			else if(!dq.isEmpty() && num != dq.peekLast()) {
+				System.out.print("NO");
+				return;
+			}
+			
+			dq.pollLast();
+//			result[rs_idx] = '-';
+			res.add("-");
+			rs_idx++;
+		}
+		
+		for(int i=0; i<rs_idx; i++) {
+			System.out.println(res.pollFirst());
+		}
+		
+	}
+}
