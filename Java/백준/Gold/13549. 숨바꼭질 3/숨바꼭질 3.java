bc8379c7f456e24d7255a9daad788053d445f94d
박희창 <122577719+prkhch@users.noreply.github.com>
2024-06-22 16:15:04 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 260 ms, Memory: 87136 KB -BaekjoonHub

---
+++ Java/백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	static int N, K;
+	static boolean[] visit; 
+	static int answer = Integer.MAX_VALUE;
+	
+	static void bfs() {
+		
+		Queue<int[]> q = new LinkedList<>();
+		
+		q.add(new int[] {N, 0});
+		
+		while(!q.isEmpty()) {
+			
+			int[] now = q.poll();
+			visit[now[0]] = true;
+			
+			if(now[0] == K) {
+				answer = Math.min(answer, now[1]);
+			}
+			
+			if(now[0]*2 <= 100000 && visit[now[0]*2] == false) {
+				q.add(new int[] {now[0]*2, now[1]});
+			}
+			
+			if(now[0]+1 <= 100000 && visit[now[0]+1] == false) {
+				q.add(new int[] {now[0]+1, now[1] + 1});
+			}
+			
+			if(now[0]-1 > 0 && visit[now[0]-1] == false) {
+				q.add(new int[] {now[0]-1, now[1] + 1});
+			}
+			
+		}
+		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		visit = new boolean[100001];
+		if(N >= K) {
+			System.out.println(N-K);
+		} else {
+			bfs();
+			System.out.println(answer);
+		}
+		
+	}
+	
+}
