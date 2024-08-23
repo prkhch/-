8be920955eea8543e8aa41ed6eba677139262736
박희창 <122577719+prkhch@users.noreply.github.com>
2024-08-23 20:53:39 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,8 +1,3 @@
 function solution(num_list) {
-    for(let i=0; i<num_list.length; i++) {
-        if(num_list[i] < 0) {
-            return i;
-        }
-    }
-    return -1;
+   return num_list.findIndex(v => v < 0)
 }
