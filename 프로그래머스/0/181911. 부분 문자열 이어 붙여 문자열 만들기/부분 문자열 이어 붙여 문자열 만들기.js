57deb502f6ea33e8c9d2dbcb961aff75577df522
박희창 <122577719+prkhch@users.noreply.github.com>
2024-08-27 10:19:50 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -1,5 +1,5 @@
 function solution(my_strings, parts) {
-    let answer = "";
-    my_strings.forEach((v,i) => answer += v.slice(parts[i][0], parts[i][1]+1));
-    return answer;
+    let answer = [];
+    my_strings.forEach((v,i) => answer.push(v.slice(parts[i][0], parts[i][1]+1)));
+    return answer.join('');
 }
