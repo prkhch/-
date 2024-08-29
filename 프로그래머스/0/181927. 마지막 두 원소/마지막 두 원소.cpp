442895a963257013aff917861c336f1fe84c3f2c
박희창 <122577719+prkhch@users.noreply.github.com>
2024-08-29 21:32:07 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.cpp
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.cpp
@@ -5,14 +5,10 @@ using namespace std;
 
 vector<int> solution(vector<int> num_list) {
     
-    int last_one = num_list[num_list.size()-1];
-    int last_two = num_list[num_list.size()-2];
+    int size = num_list.size();
     
-    if(last_one > last_two) {
-        num_list.push_back(last_one - last_two);
-    } else {
-        num_list.push_back(last_one * 2);
-    }
+    num_list[size-1] > num_list[size-2] ? num_list.push_back(num_list[size-1] - num_list[size-2]) : num_list.push_back(num_list[size-1] * 2);
     
     return num_list;
+    
 }
