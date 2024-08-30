26ad7d5819e201fff0afeddf751f296aff2a1c3e
박희창 <122577719+prkhch@users.noreply.github.com>
2024-08-30 20:35:26 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.03 ms, Memory: 3.71 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.cpp
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.cpp
@@ -6,12 +6,8 @@ using namespace std;
 int solution(vector<int> num_list) {
     string one = "";
     string two = "";
-    for(int i=0; i<num_list.size(); i++) {
-        if(num_list[i]%2 == 0) {
-            two += to_string(num_list[i]);
-        } else {
-            one += to_string(num_list[i]);
-        }
+    for(int i : num_list) {  
+        i%2 == 0 ? two+=to_string(i) : one+=to_string(i); 
     }
     return stoi(one) + stoi(two);
 }
