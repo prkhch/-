624db866f233c35d4ec3dc5a6dd18c7b1061aa94
박희창 <122577719+prkhch@users.noreply.github.com>
2024-09-17 17:23:49 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.cpp
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.cpp
@@ -4,17 +4,11 @@
 using namespace std;
 
 int solution(vector<int> num_list) {
-    int answer = 0;
-    
-    int odd = 0;
-    int even = 0;
+    int odd = 0, even = 0;
     
     for(int i=0; i<num_list.size(); i++) {
-        if((i+1)%2 == 1) {
-            odd += num_list[i];
-        } else {
-            even += num_list[i];
-        }
+        (i % 2 == 0 ? odd : even) += num_list[i];
     }
-    return odd >= even ? odd : even;
+    
+    return max(odd, even);
 }
