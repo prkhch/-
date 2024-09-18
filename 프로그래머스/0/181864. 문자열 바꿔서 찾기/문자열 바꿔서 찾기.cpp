d2dbb95dea65e2124a2c3233bc359efaf48022cf
박희창 <122577719+prkhch@users.noreply.github.com>
2024-09-18 22:45:53 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.01 ms, Memory: 3.73 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.cpp
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.cpp
@@ -4,9 +4,6 @@
 using namespace std;
 
 int solution(string myString, string pat) {
-    int answer = 0;
-    for(int i=0; i<myString.size(); i++) {
-        myString[i] = myString[i] == 'A' ?  'B' : 'A';
-    }  
+    for(char &c : myString) c = c == 'A' ? 'B' : 'A';
     return myString.find(pat) == string::npos ? 0 : 1;
 }
