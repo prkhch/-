17d9eb99bf6677e10af2809e6e52c2bcf1e3fe53
박희창 <122577719+prkhch@users.noreply.github.com>
2024-09-18 17:11:36 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.02 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cpp
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cpp
@@ -5,13 +5,7 @@
 using namespace std;
 
 vector<int> solution(vector<int> num_list) {
-    vector<int> answer;
-    
-    sort(num_list.begin(), num_list.end());
-    
-    for(int i=5; i<num_list.size(); i++) {
-        answer.push_back(num_list[i]);
-    }
-    
+    sort(num_list.begin(), num_list.end());   
+    vector<int> answer(num_list.begin() + 5, num_list.end());
     return answer;
 }
