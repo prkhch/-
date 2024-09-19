09e7c9b08b1e0450203b9b7c884a8235784fcf99
박희창 <122577719+prkhch@users.noreply.github.com>
2024-09-19 22:34:25 +0900

    [level 0] Title: rny_string, Time: 0.05 ms, Memory: 4.06 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.cpp
+++ 프로그래머스/0/181863. rny＿string/rny＿string.cpp
@@ -7,8 +7,8 @@ string solution(string rny_string) {
     
     size_t pos = 0;
     
-    while(rny_string.find("m", 0) != string::npos) {
-        pos = rny_string.find("m", 0);
+    while(rny_string.find("m") != string::npos) {
+        pos = rny_string.find("m");
         rny_string.replace(pos, 1, "rn");
     }
     
