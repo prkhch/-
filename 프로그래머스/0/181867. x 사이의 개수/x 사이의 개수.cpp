21a57550d593911fc0491dc43cbdc1cf0d9af7c6
박희창 <122577719+prkhch@users.noreply.github.com>
2024-09-21 15:10:10 +0900

    [level 0] Title: x 사이의 개수, Time: 5.26 ms, Memory: 7.21 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.cpp
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.cpp
@@ -6,11 +6,9 @@ using namespace std;
 vector<int> solution(string myString) {
     vector<int> answer;
     
-    size_t pos = 0;
-    size_t st = 0;
+    size_t pos = 0, st = 0;
     
-    while(myString.find('x', st) != string::npos) {
-        pos = myString.find('x', st);
+    while((pos = myString.find('x', st)) != string::npos) {
         answer.push_back(pos - st);
         st = pos+1;
     }
