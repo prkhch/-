453c4b9f91e7e79769c5c6ac1561caa4091f6a5e
박희창 <122577719+prkhch@users.noreply.github.com>
2024-09-21 20:24:51 +0900

    [level 0] Title: 369게임, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int order) {
+    int answer = 0;
+    
+    string str = to_string(order);
+    
+    for(int i=0; i<str.length(); i++) {
+        if(str[i] == '3' || str[i] == '6' || str[i] == '9') answer++;
+    }
+    
+    return answer;
+}
