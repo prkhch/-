19485f99da8bb1d08acb68b75b0f5305972369ed
박희창 <122577719+prkhch@users.noreply.github.com>
2024-09-22 01:51:53 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    for(int i=1; i<=n*6; i++) {
+        if(i*6%n == 0) {
+            return i;
+        }
+    }
+}
