bb5787c3a6b41e9fc9688f7f56b2e0f57c2b4131
박희창 <122577719+prkhch@users.noreply.github.com>
2024-09-22 09:14:32 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.cpp
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.cpp
@@ -9,13 +9,9 @@ int solution(int n) {
         
         int cnt = 0;
         
-        for(int j=1; j*j<=i; j++) {
-            if(i%j == 0) {
-                cnt++;
-                if(i != j) {
-                    cnt++;
-                }
-            }   
+        for(int j=1; j<=i; j++) {
+            if(i%j == 0) cnt++;
+            if (cnt >= 3) break;
         }
         
         if(cnt >= 3) answer++;
