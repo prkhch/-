cd276e753acd396a5bc4d31ee6de87e75c7aba89
박희창 <122577719+prkhch@users.noreply.github.com>
2024-10-14 20:15:01 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    
+    int alphabet[26] = {};
+    
+    for(char c : s) alphabet[c-'a']++;
+    
+    for(int i=0; i<26; i++)
+        if(alphabet[i] == 1)
+            answer += i+'a';
+     
+    return answer;
+}
