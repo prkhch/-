d93f21215b7c66a7a8d56ef16fe7ee57fc30a013
박희창 <122577719+prkhch@users.noreply.github.com>
2025-03-16 20:36:55 +0900

    [level 1] Title: 추억 점수, Time: 1.65 ms, Memory: 4.29 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    vector<int> answer;
+    
+    map<string, int> m;
+    for(int i=0; i<name.size(); i++) {
+        m[name[i]] = yearning[i];
+    }
+    
+    for(int i=0; i<photo.size(); i++) {
+        int pt_score = 0;
+        for(int j=0; j<photo[i].size(); j++) {
+            pt_score += m[photo[i][j]];
+        }
+        answer.push_back(pt_score);
+    }
+    
+    return answer;
+}
