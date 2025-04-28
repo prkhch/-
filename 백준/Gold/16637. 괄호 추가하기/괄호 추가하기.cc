f47e1a4a65c0aedaaaff9d05ba758371b535805c
prkhch <shjc4623@gmail.com>
2025-04-28 21:23:11 +0900

    [Gold III] Title: 괄호 추가하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+string s;
+int mx = -987654321;
+
+int cal(int a, char op, int b) {
+    if(op=='+') {
+        return a + b;
+    } else if(op=='-') {
+        return a - b;
+    } else if (op=='*') {
+        return a * b;
+    }
+}
+
+void dfs(int sum, int pos) {
+    if(pos >= N) {
+        mx = max(mx, sum);
+        return;
+    }
+
+    // 앞 괄호
+    if (pos + 1 < N) {
+        dfs(cal(sum, s[pos], s[pos + 1] - '0'), pos + 2);
+    }
+    
+    // 뒷 괄호
+    if (pos + 3 < N) {
+        int bracket = cal(s[pos + 1] - '0', s[pos + 2], s[pos + 3] - '0');
+        dfs(cal(sum, s[pos], bracket), pos + 4);
+    }
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    cin >> N >> s;
+    dfs(s[0]-'0', 1);
+    
+    cout << mx;
+
+}
