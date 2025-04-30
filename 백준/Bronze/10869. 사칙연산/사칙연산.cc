fd9d947eb1a8767c179bc973bac7d46d9b8284be
prkhch <shjc4623@gmail.com>
2025-04-30 10:24:00 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int A, B;
+
+int main() {
+    cin >> A >> B;
+    cout << A+B << "\n";
+    cout << A-B << "\n";
+    cout << A*B << "\n";
+    cout << A/B << "\n";
+    cout << A%B << "\n";
+}
