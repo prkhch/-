43c6e26eeab3c0d5967b7d050c030eccf27fc3fb
prkhch <shjc4623@gmail.com>
2025-08-14 00:20:41 +0900

    4/1000

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
+// 4
 // https://pixx.tistory.com/85
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs
-  .readFileSync(filePath)
-  .toString()
-  .split("\n")
-  .map((v) => v.split(" ").map(Number));
+let input = fs.readFileSync(filePath).toString().trim();
 
-let [A, B, C] = input.shift();
-
-let arr = Array(101).fill(0);
-
-for (const i of input) {
-  for (let j = i[0]; j < i[1]; j++) {
-    arr[j]++;
+for (let i = 0; i < input.length / 2; i++) {
+  if (input.charAt(i) !== input.charAt(input.length - i - 1)) {
+    console.log(0);
+    process.exit(0);
   }
 }
 
-let result = 0;
-for (const i of arr) {
-  result += i === 1 ? A : i === 2 ? 2 * B : i === 3 ? 3 * C : 0;
-}
-
-console.log(result);
+console.log(1);
