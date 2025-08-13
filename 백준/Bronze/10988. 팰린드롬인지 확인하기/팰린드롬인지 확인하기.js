1728120fec5f1c67103a6902a2f439037ce9d22a
prkhch <shjc4623@gmail.com>
2025-08-14 00:19:39 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 96 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.js
@@ -0,0 +1,13 @@
+// https://pixx.tistory.com/85
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim();
+
+for (let i = 0; i < input.length / 2; i++) {
+  if (input.charAt(i) !== input.charAt(input.length - i - 1)) {
+    console.log(0);
+    process.exit(0);
+  }
+}
+
+console.log(1);
