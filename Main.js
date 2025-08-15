1023961e36229e20daba30f0b5c212374ed23e45
prkhch <shjc4623@gmail.com>
2025-08-15 22:43:48 +0900

    7/1000

--- Main.js
+++ Main.js
@@ -1,24 +1,25 @@
-// 6
+// 7
 // https://pixx.tistory.com/85
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().split("\n");
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-let answer = "";
-for (let i = 0; i < input[0].length; i++) answer += solution(input[0][i]);
+const N = input.shift();
+const pattern = input.shift();
+const [st, ed] = pattern.trim().split("*");
+for (let i = 0; i < N; i++) {
+  //https://www.acmicpc.net/board/view/125380
+  if (input.length < st.length + ed.length) {
+    console.log("NE");
+    continue;
+  }
 
-console.log(answer);
-
-function solution(char) {
-  if (97 <= char.charCodeAt(0) && char.charCodeAt(0) <= 97 + 25) {
-    if (char.charCodeAt(0) + 13 > 97 + 25)
-      return String.fromCharCode(char.charCodeAt(0) + 13 - 26);
-    else return String.fromCharCode(char.charCodeAt(0) + 13);
-  } else if (65 <= char.charCodeAt(0) && char.charCodeAt(0) <= 65 + 25) {
-    if (char.charCodeAt(0) + 13 > 65 + 25)
-      return String.fromCharCode(char.charCodeAt(0) + 13 - 26);
-    else return String.fromCharCode(char.charCodeAt(0) + 13);
+  if (
+    st === input[i].substring(0, st.length) &&
+    ed === input[i].substring(input[i].length - ed.length)
+  ) {
+    console.log("DA");
   } else {
-    return char;
+    console.log("NE");
   }
 }
